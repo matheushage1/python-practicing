24b63a624061a1934ebc92256ad9770e74e2721d
matheushage1 <matheushagecontato@gmail.com>
2023-01-05 23:10:34 -0300

    some exercies about vars with python

---
+++ class1-vars.py
@@ -0,0 +1,7 @@
+# calculating the working hours of an employee
+
+monthly_payment = input("How much do you receive? ")
+working_hours = input("How many hours did you worked? ")
+paycheck = int(monthly_payment) / int(working_hours)
+
+print(paycheck)
