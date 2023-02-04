f283d8d6a80564cefdead4eaefb8dae082160e4c
matheushage1 <matheushagecontato@gmail.com>
2023-02-03 21:00:54 -0300

    keeping practicing some python stuff.

--- python-first-world-1/class-04-challenge-03.py
+++ python-first-world-1/class-04-challenge-03.py
@@ -1,6 +1,6 @@
-firstNumber = input('Please, put some number in here, the first of two: ')
-secondNumber = input('Please, put some number in here, the two of two: ')
+firstNumber = int(input('Please, put some number in here, the first of two: '))
+secondNumber = int(input('Please, put some number in here, the two of two: '))
 
-concatenatingBothNumbers = firstNumber + secondNumber
+sumOfBothNumbers = firstNumber + secondNumber
 
-print('The sum of the numbers is: ', concatenatingBothNumbers)
+print('The sum of the numbers is: ', sumOfBothNumbers)
---
+++ python-first-world-1/class-06-primitive-types.py
@@ -0,0 +1,8 @@
+randomStuff = input('Type something here, please: ')
+
+print('the primitive type of the variable is: ', type(randomStuff))
+print('is this in uppercase?', randomStuff.isupper())
+print('is this in lowercase?', randomStuff.islower())
+print('is this a alphanumeric?', randomStuff.isalnum())
+print('is this only alpha? ', randomStuff.isalpha())
+print('is this printable? ', randomStuff.isprintable())
